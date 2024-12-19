8dc977f3b7bfa244f75fd4adb87a730469503c4c
KarsteenPW <karsteenpw@gmail.com>
2024-12-19 19:46:42 +0700

    Sword damage and skin
    Sejujurnya aku tak mengerti kenapa yang diupload banyak bgt, tapi anu buat damage itu script Sword di taro di sword masing2 nnti di inspector tinggal set dmg nya, tp itu script ud gw masukkin ke semuanya yang di folder "Swords", and for the skin itu kekny yg mnrt gw paling bagus dari semua pilihan yang ada

--- PawQuest/Assets/Scripts/Hero/Weapon.cs
+++ PawQuest/Assets/Scripts/Hero/Weapon.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    private Sword currentSword; // Reference to the current sword
 
-    public void OnTriggerEnter(Collider  other) {
-		if(other.gameObject.tag == "Enemy"){
+    private void Start()
+    {
+        // Get the Sword component attached to the Weapon GameObject
+        currentSword = GetComponentInChildren<Sword>();
+    }
 
-			HeroAttribute hero = GameObject.Find("Hero").GetComponent<HeroAttribute>();
-			HeroController heroController = GameObject.Find("Hero").GetComponent<HeroController>();
-			Enemy enemy = other.gameObject.GetComponent<Enemy>();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            HeroAttribute hero = GameObject.Find("Hero").GetComponent<HeroAttribute>();
+            HeroController heroController = GameObject.Find("Hero").GetComponent<HeroController>();
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
 
-			
             if (enemy != null && hero != null && heroController.isAttacking)
             {
-                // Call the TakeDamage method and pass the damage value
-                enemy.TakeDamage(hero.damage);
-				Debug.Log("Hit!!!" + hero.damage);
+                // Use the sword's damage value
+                int damage = currentSword != null ? currentSword.damage : hero.damage; // Fallback to hero's damage if sword is null
+                enemy.TakeDamage(damage);
+                Debug.Log("Hit!!! Damage dealt: " + damage);
             }
-		}
-	}
+        }
+    }
 }
